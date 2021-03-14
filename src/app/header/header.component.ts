e05fd2098f2d851cf0db5416f17431fb02f2c948
Jetniphan Pukkham <jeni8399@gmail.com>
2021-03-14 16:24:48 +0700

    addsidenav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+  @Output() navtoggle = new EventEmitter();
   demoMailNoti = 50;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClickNavToggle(){
+    this.navtoggle.emit();
   }
 
 }
