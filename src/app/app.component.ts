d74248c8aefe9866df2fb7e7c217b61d165ee5f2
Jetniphan Pukkham <jeni8399@gmail.com>
2021-03-14 19:22:25 +0700

    addmotionmobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ThisReceiver } from '@angular/compiler';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular';
+export class AppComponent implements OnDestroy {
+  mobileQueryMax: MediaQueryList;
+  private _mobileQueryListener: () => void;
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQueryMax = window.matchMedia('(max-width: 800px)');
+    this.mobileQueryMax.addListener(this._mobileQueryListener);
+
+
+  }
+  ngOnDestroy(): void {
+    this.mobileQueryMax.removeListener(this._mobileQueryListener);
+  }
 }
