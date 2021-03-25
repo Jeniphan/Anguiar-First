600e60371943e2e80006e4c956cecd52f087c479
Jetniphan Pukkham <jeni8399@gmail.com>
2021-03-26 02:08:10 +0700

    add moblieQueryMax

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,12 @@ import { Component, OnInit, Output, EventEmitter} from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  @Output() navtoggle = new EventEmitter();
+  
+  @Input("medie_query") mobileQueryMax: boolean;
+  @Output("toggle") navtoggle = new EventEmitter();
   demoMailNoti = 50;
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
