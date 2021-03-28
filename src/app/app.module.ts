c25c75194b8fafb5babafeb1b448a3c47bab80ad
Jetniphan Pukkham <jeni8399@gmail.com>
2021-03-28 17:04:46 +0700

    add table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HeaderComponent } from './header/header.component';
+import { StockHomeComponent } from './stock/stock-home/stock-home.component';
+import { StockCreateComponent } from './stock/stock-create/stock-create.component';
+import { StockEditComponent } from './stock/stock-edit/stock-edit.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { SideNavComponent } from './side-nav/side-nav.component';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
-import { StockHomeComponent } from './stock/stock-home/stock-home.component';
-import { StockCreateComponent } from './stock/stock-create/stock-create.component';
-import { StockEditComponent } from './stock/stock-edit/stock-edit.component';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -42,7 +42,8 @@ import { MatCardModule } from '@angular/material/card';
     MatSidenavModule,
     MatListModule,
     MatDividerModule,
-    MatCardModule
+    MatCardModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/stock/stock-home/stock-home.component.ts
+++ src/app/stock/stock-home/stock-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-stock-home',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockHomeComponent implements OnInit {
 
+  displayColumns = ['image', 'name', 'price', 'stock', 'action'];
+
+  dataSource = new MatTableDataSource<Product>();
+
   constructor() { }
 
   ngOnInit(): void {
+    this.feedData();
   }
 
+  feedData() {
+    const dummy: Product[] = [{
+      name: "mac book",
+      stock: 10,
+      price: 900,
+      image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-gold-select-201810?wid=892&hei=820&&qlt=80&.v=1603332211000"
+    },
+    {
+      name: "mac book",
+      stock: 9,
+      price: 900,
+      image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-gold-select-201810?wid=892&hei=820&&qlt=80&.v=1603332211000"
+    },
+    {
+      name: "mac book",
+      stock: 8,
+      price: 900,
+      image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-gold-select-201810?wid=892&hei=820&&qlt=80&.v=1603332211000"
+    },
+    {
+      name: "mac book",
+      stock: 10,
+      price: 900,
+      image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-gold-select-201810?wid=892&hei=820&&qlt=80&.v=1603332211000"
+    },
+    {
+      name: "mac book",
+      stock: 7,
+      price: 900,
+      image: "https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-gold-select-201810?wid=892&hei=820&&qlt=80&.v=1603332211000"
+    },
+    ]
+    this.dataSource.data = dummy
+  }
 }
